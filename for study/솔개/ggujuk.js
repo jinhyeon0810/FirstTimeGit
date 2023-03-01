375e33f11a38496e7e0ccacd0cb05e9454402d33
jinhyeon0810 <jinhyeon0810@gmail.com>
2023-03-01 22:01:43 +0900

    for study JS commit

---
+++ for study/솔개/ggujuk.js
@@ -0,0 +1,20 @@
+const babe =[1,2,3,'4']
+
+console.log(babe.indexOf(2)); // 2의 인덱스 위치
+ 
+console.log(babe[2]); //배열에서 3번째의 값 (0,1,2..), index 2의 값
+
+
+const drumSounds = [
+    { key: 81, sound: 'clap.wav' },
+    { key: 87, sound: 'crash.wav' },
+    { key: 69, sound: 'hihat.wav' },
+    { key: 65, sound: 'kick.wav' },
+    { key: 83, sound: 'openhat.wav' },
+    { key: 68, sound: 'ride.wav' },
+    { key: 90, sound: 'shaker.wav' },
+    { key: 88, sound: 'snare.wav' },
+    { key: 67, sound: 'tom.wav' },
+  ]
+
+console.log(drumSounds)
