a0b66328cd2bf2bf08ca03b4b20ac5439e046f4b
jinhyeon0810 <jinhyeon0810@gmail.com>
2023-03-06 22:16:54 +0900

    codingApple modal Javascript basic html css study commit

--- for study/솔개/codingApple/modal/modal.js
+++ for study/솔개/codingApple/modal/modal.js
@@ -57,3 +57,68 @@ darkBtn.addEventListener('click', ()=>{
     }
 })
 
+///set Time out! //여기서 getelementbyclassname 이 아닌 bytagname으로 해서 20분 헤맴;
+const alertspan = document.getElementsByClassName('alertspan')[0];
+const alertdiv = document.getElementsByClassName('alert')[0];
+setTimeout(()=>{
+    alertspan.innerHTML = '4';
+},1000);
+
+setTimeout(()=>{
+    alertspan.innerHTML = '3';
+},2000);
+
+setTimeout(()=>{
+    alertspan.innerHTML = '2';
+},3000);
+
+setTimeout(()=>{
+    alertspan.innerHTML = '1';
+},4000);
+
+setTimeout(()=>{
+    alertdiv.style.display='none';
+},5000);
+
+//==========캐러셀 동작 구현
+
+const btn1 = document.getElementsByClassName('btn1')[0];
+const btn2 = document.getElementsByClassName('btn2')[0];
+const btn3 = document.getElementsByClassName('btn3')[0];
+const slideCon = document.getElementsByClassName('slide-container')[0];
+const next = document.getElementsByClassName('next')[0];
+
+
+btn1.addEventListener('click', ()=>{
+    slideCon.style.transform = 'none';
+    지금사진 =1;
+})
+
+btn2.addEventListener('click', ()=>{
+    slideCon.style.transform = 'translateX(-100vw)';
+})
+
+btn3.addEventListener('click', ()=>{
+    slideCon.style.transform = 'translateX(-200vw)';
+})
+
+
+//다음버튼 누르면 지금 보이는 사진이 1이면 2번 사진보여주세요
+// 지금 보이는 사진이 2면 3번 보여주세요
+
+let 지금사진 = 1;
+
+next.addEventListener('click', ()=>{
+    if(지금사진 == 1){
+     slideCon.style.transform = 'translateX(-100vw)';
+     지금사진 = 2;
+    } else if
+    (지금사진 == 2){
+    slideCon.style.transform = 'translateX(-200vw)';
+    지금사진 = 3;
+    }
+    console.log(지금사진);
+})
+
+
+
