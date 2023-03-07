829989af584bfcde94a23bf534685d2289050d77
jinhyeon0810 <jinhyeon0810@gmail.com>
2023-03-07 21:10:19 +0900

    codingApple menu modal javascript basic study commit

--- for study/솔개/codingApple/modal/modal.js
+++ for study/솔개/codingApple/modal/modal.js
@@ -121,4 +121,17 @@ next.addEventListener('click', ()=>{
 })
 
 
+blackBg.addEventListener('click', (e)=>{
+   
+    // e.target; //유저가 실제로 누른거
+    // e.currentTarget; //이벤트리스너 달린 곳 (여기선 blackBg): this;
+    // e.preventDefault() -> //이벤트 기본동작 막아주삼
+    // e.stopPropagation(); // 내 상위요소로 퍼지는 이벤트 버블링 막음
+    console.log(e.target);
+    if(e.target == blackBg){
+    blackBg.classList.remove('show');
+}
+
+})
+
 
