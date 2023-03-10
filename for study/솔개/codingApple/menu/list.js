573b060ec4f2f804d0a12afff64d1bb05354289c
jinhyeon0810 <jinhyeon0810@gmail.com>
2023-03-10 22:29:20 +0900

    codingApple javascript map filter sort basic study commit

--- for study/솔개/codingApple/menu/list.js
+++ for study/솔개/codingApple/menu/list.js
@@ -40,12 +40,56 @@ data.forEach((a)=>{
 
 //더보기 버튼 2번 누르면 7,8,9번째 상품 더 가져오고 , 버튼 안보이게하기
 
-const btnDanger = document.getElementsByClassName('btn-danger')[0];
+const sort1 = document.getElementById('sort1');
 
-btnDanger.addEventListener('click', ()=>{
+sort1.addEventListener('click', ()=>{
     products.sort((a, b)=>{
-        return a.price -b.price;
-       
+        if(a.title < b.title){
+            return 1
+        } else {
+            return -1
+        }
+     
     })
     console.log(products);
+
+        row.innerHTML = '';
+        
+    products.forEach((a)=>{
+        let 템플릿 = `<div class="col-sm-4">
+         <img src="https://via.placeholder.com/600" class="w-100">
+         <h5>${a.title}</h5>
+         <p> 가격 : ${a.price}</p>
+         </div>`
+         
+         row.insertAdjacentHTML('beforeend',템플릿);;
+       
+       }) 
+    
 })
+
+ document.getElementById('filter').addEventListener('click', ()=>{
+    //filter 쓸때 새로운 변수에 담아 써야함 (데이터가 안변하므로)
+    let newProduct = products.filter((a)=>{
+        return a.price <=60000;
+    });
+    row.innerHTML = '';
+        
+    newProduct.forEach((a)=>{
+        let 템플릿 = `<div class="col-sm-4">
+         <img src="https://via.placeholder.com/600" class="w-100">
+         <h5>${a.title}</h5>
+         <p> 가격 : ${a.price}</p>
+         </div>`
+         
+         row.insertAdjacentHTML('beforeend',템플릿);;
+       
+       }) 
+
+    
+    console.log(newProduct);
+ })
+
+
+
+
