cea30978a7e27edf294a3cc1d2ba7fc157af548f
jinhyeon0810 <jinhyeon0810@gmail.com>
2023-04-03 22:11:46 +0900

    React Study Commit

---
+++ newREACT/new/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+import OnlineStore from './components/OnlineStore';
+
+
+function App() {
+  return(
+      <OnlineStore/>
+  )
+  
+
+}
+
+export default App;
